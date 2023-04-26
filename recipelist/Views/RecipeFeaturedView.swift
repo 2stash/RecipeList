78ef196bdc3b29a00e4117e6d42b2111452d01e7
Dan <dwestlund222@hotmail.com>
2023-04-26 10:11:03 -0500

    -add RecipeFeatureView -modify data flow using environmentobject modifier

---
+++ recipelist/Views/RecipeFeaturedView.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeFeaturedView.swift
+//  recipelist
+//
+//  Created by daniel westlund on 4/26/23.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
